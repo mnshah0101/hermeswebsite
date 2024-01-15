72bf9405661f16d0d87abed702ea1c8bdea85af1
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-01-15 15:08:08 -0500

    no-testimonials

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ import AboutUs from '@/components/AboutUs/AboutUs3';
 import Works from '@/components/Works/Works3';
 import Services from '@/components/Services/Services3';
 import MinimalArea2 from '@/components/Others/MinimalArea2';
-import FullTestimonials from '@/components/Testimonials/FullTestimonials';
 import Team from '@/components/Team/Team1';
 import Blogs from '@/components/Blogs/Blogs4';
 import CallToAction from '@/components/Others/CallToAction';
@@ -45,7 +44,6 @@ export default function LandingPreview() {
       <Works />
       <Services />
       <MinimalArea2 />
-      <FullTestimonials classText="pb-0" showHead={true} />
       <Team />
       <Blogs />
       <CallToAction />
